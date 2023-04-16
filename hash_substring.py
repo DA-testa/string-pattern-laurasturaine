4f7c4ee5b70ac41e438c89e2641135b8efe2118f
laurasturaine <125255157+laurasturaine@users.noreply.github.com>
2023-04-16 19:21:46 +0000

    upd1

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,61 @@
-# python3
-
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    # Read pattern and text from input
+    pattern = input().rstrip()
+    text = input().rstrip()
     
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
+    # Print occurrences in ascending order
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    # Implement Rabin-Karp algorithm to find occurrences of pattern in text
 
-    # and return an iterable variable
-    return [0]
+    def rabin_karp(pattern, text):
+        # Constants for Rabin-Karp algorithm
+        d = 256  # Number of characters in the input alphabet
+        q = 101  # A prime number
+
+        m = len(pattern)
+        n = len(text)
+        p = 0  # Hash value for pattern
+        t = 0  # Hash value for text
+        h = 1
+
+        occurrences = []  # List to store occurrences of the pattern
+
+        # Calculate hash value of pattern and first window of text
+        for i in range(m):
+            p = (d * p + ord(pattern[i])) % q
+            t = (d * t + ord(text[i])) % q
+
+        # Calculate the value of h, which will be used to remove the leftmost character from the hash value
+        for i in range(m - 1):
+            h = (h * d) % q
+
+        # Slide the pattern over the text one by one
+        for i in range(n - m + 1):
+            # Check if hash values of current window of text and pattern match
+            if p == t:
+                # Check if characters in current window and pattern match
+                for j in range(m):
+                    if text[i + j] != pattern[j]:
+                        break
+                else:
+                    occurrences.append(i)  # Add index of occurrence to list
+
+            # Calculate hash value for the next window of text
+            if i < n - m:
+                t = (d * (t - ord(text[i]) * h) + ord(text[i + m])) % q
+
+        return occurrences
+
+    return rabin_karp(pattern, text)
 
 
-# this part launches the functions
+# This part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
+
 
